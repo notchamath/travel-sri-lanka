34b2d0e21e832448e5324ff66e8fd90603dc3c8f
Chamath Jayarathne <chamathsocial@gmail.com>
2024-05-15 18:17:56 -0400

    fix: trying out new google maps api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import GoogleMap from "./components/Map/Map"
+import {APILoader, PlaceOverview} from '@googlemaps/extended-component-library/react';
+import React from 'react';
+import './App.css';
+
 
 function App() {
 
@@ -6,6 +10,13 @@ function App() {
     <>
       <h1>Hello, world</h1>
       <GoogleMap/>
+      <div className="container">
+        {/* <APILoader 
+          apiKey="YOUR_API_KEY_HERE"
+          solutionChannel="GMP_GCC_placeoverview_v1_xl" 
+        /> */}
+        <PlaceOverview place="ChIJs5pMcluh_DoRUg03WydxF6s" />
+      </div>
     </>
   )
 }
